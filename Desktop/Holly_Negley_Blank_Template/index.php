011f2838e37097cd1b281b69aefe53b9813cfe83
Holly Negley <holly.negley@icloud.com>
2015-06-07 22:28:20 -0400

    added content, linked files, added css folder aka skeleton

--- Desktop/Holly_Negley_Blank_Template/index.php
+++ Desktop/Holly_Negley_Blank_Template/index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title></title>
+	<title></title>
+
+	<!-- Links to our Style.css file -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+
 </head>
 <body>
-    <h1>Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
-</body>
+	<div class="container">
+		
+		<header class="row">
+			<div class="twelve columns">
+    			<h1>Blank WordPress Template</h1>
+    			<p>This is my WordPress template.</p>
+    		</div>	
+    	</header>
+    	
+    	<section class="row">
+    		<div class="twelve columns">
+    			<p>Body content goes here.</p>
+    		</div>
+    	</section>
+    	
+    	<footer class="row">
+    		<div class="twelve columns">
+    			<P>Footer content goese here.</P>
+    		</div>	
+    	</footer>
+    	
+    </div> <!--ends container-->		
 </body>
 </html>
