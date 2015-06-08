cb1c99558d6b112a243f85cc70824583d4d58165
Holly Negley <holly.negley@icloud.com>
2015-06-08 14:36:30 -0400

    added the menu

--- Desktop/Holly_Negley_Blank_Template/functions.php
+++ Desktop/Holly_Negley_Blank_Template/functions.php
@@ -52,4 +52,11 @@ function blank_widgets_init() {
     }
 add_action('widgets_init', 'blank_widgets_init');
 
+/*-------------- Enable Menu --------------- */
+
+add_theme_support('menus');
+
+
+
+
 
--- Desktop/Holly_Negley_Blank_Template/header.php
+++ Desktop/Holly_Negley_Blank_Template/header.php
@@ -8,10 +8,21 @@
 </head>
 <body>
 	<div class="container">
+	
 	<header class="row">
+			
 			<div class="twelve columns">
     			<h1><?php bloginfo('name'); ?></h1>
                 <p><?php bloginfo('description'); ?></p>
+    		
     		</div>	
     	</header>
+    	<div class="row">
+    		<div class="twelve columns">
+    			<?php wp_nav_menu(array (
+    				'sort_column' => 'menu_order',
+    				'container_class' => 'blank-menu-header'
+
+    				)); ?>
+
 <!--END OF HEADER AREA-->
